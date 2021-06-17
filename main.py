bdeed69f8c979a1828bab8b9ca4087e5c1c27542
Jordan Onwodi <81950260+Jonwodi@users.noreply.github.com>
2021-06-17 14:39:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+""" Step 1: check to see if you have python installed on your computer system. """
+
+""" Step 2: to do this open windows powershell, command prompt or git bash by going to the start menu and typing in windows powershell, command prompt or git bash and then clicking open. """
+
+""" Step 3: to download git bash use this link: https://git-scm.com/downloads """
+
+""" Step 4: then type python in all lowercases into the terminal your using."""
+
+""" Step 5: if you get a response like this Python 3.9.5 or like this Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32 Type "help", "copyright", "credits" or "license" for more information. It means that python is already installed on your computer system."""
+
+""" Step 6: if you get an error message saying that python is not a recognized command. python isn't installed. It just means that you don't have python on your computer system and that you need to dowmload it onto your computer system."""
+
+""" Step 7: also if you have a python version that is earlier than python 3.6, you should also download the latset release of python. """
+
+""" Step 8: to download python for your windows computer system go to htStep 1:tps://www.python.org/ and hover over the Downlaods link. You should see a button dor downlaoding the latest version of python for your computer system. Click the button, which should automatically start downlaoding a python version that is compatiable with your computer system. """
+
+""" Step 9: once the python file has finished downloading, open it up and click run. Finally, before you install python onto your computer system, make sure to select the option 'Add Python to PATH'. Then click install. """
+
+""" Step 10: repeat steps 2 and 4. You should get response similar to step 5, once you entered python into your terminal. """
+
+""" Step 11: Now type, print("Hello Python interpreter!"), into your terminal and you should get a response or string saying Hello Python interpreter! """
